1a98622bd16dfd2a1d801f0866ba12645dd92534
MuhamadAgus <113649505+MhmdAgus21@users.noreply.github.com>
2023-05-01 09:48:56 +0700

    Update MainController.php

--- Praktikum5/app/Http/Controllers/MainController.php
+++ Praktikum5/app/Http/Controllers/MainController.php
@@ -10,7 +10,7 @@ class MainController extends Controller
     {
         $data = array();
         $data['title'] = "Blade Directive";
-        $data['npm'] = 212310022;
+        $data['npm'] = 212310004;
         $skill[] = array("course"=>"Matematika", "type"=>"Diskrit", "rate"=>4);
         $skill[] = array("course"=>"Matematika", "type"=>"Aljabar Linear", "rate"=>3);
         $skill[] = array("course"=>"Basis Data", "type"=>"DDL", "rate"=>2);
